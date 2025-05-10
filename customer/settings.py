392c68328400dec37689afe23a65f909953096b9
Shashan223 <shashanksingh9945@gmail.com>
2025-05-10 16:47:16 +0530

    Update project files

--- customer/settings.py
+++ customer/settings.py
@@ -119,12 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS=[
-STATIC_DIR,
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# (Optional but recommended)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
