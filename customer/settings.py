f87545c1c7c9e96447b57a1c9a39cedb4759c35d
Shashan223 <shashanksingh9945@gmail.com>
2025-06-03 14:36:05 +0530

    Your commit message here

--- customer/settings.py
+++ customer/settings.py
@@ -13,6 +13,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or any directory you prefer
+
 
 
 # Quick-start development settings - unsuitable for production
